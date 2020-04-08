6cf40d43c0ff0874f35dbbf64c24bd584316ff6d
Mohiuddin Ahmed <mohiuddin_ahmed_07@yahoo.com>
2020-04-08 20:52:14 +0600

    api support for jokes

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,6 +3,10 @@
 namespace Apsbd\BanglaJokes\Tests;
 
 use Apsbd\BanglaJokes\JokeFactory;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
@@ -10,24 +14,18 @@ class JokeFactoryTest extends TestCase
     /** @test */
     public function it_returns_random_bangal_joke()
     {
-        $jokes = new JokeFactory(['A bangla joke']);
-        $this->assertSame('A bangla joke', $jokes->getRandomJoke());
-    }
+        $mock = new MockHandler(
+            [
+                new Response(200, [], '{ "type": "success", "value": { "id": 462, "joke": "Project managers never ask Chuck Norris for estimations... ever.", "categories": ["nerdy"] } }'),
+            ]
+        );
 
-    /** @test */
-    public function it_returns_predefined_random_bangla_joke()
-    {
-        $banglaJokes = [
-            'যখন তোমার একা লাগবে, তুমি চারদিকে কিছুই দেখতে পাবে না, দুনিয়া টা ঝাপসা হয়ে আসবে। তখন তুমি আমার কাছে এসো। . . তোমাকে চোখের ডাক্তার দেখাবো।',
-            'এখন আমার হাতে এক বোতল বিশ। আমি মুক্তি পেতে চাই এতো জালা আমার আর এখন সহ্য হয় না। জানি এটা পাপ। এতো যন্ত্রণা আর ভালো লাগে না। তাই যাচ্ছি ইদুর মারতে।',
-            'সে আসলো, আমার উপর বসলো, আমাকে জড়িয়ে ধরলো, পরে কামর, চুমু দিল। তারপর নিজের প্রয়োজন মিটিয়ে চলে গেল। খারাপ চিন্তা ভাবনা বাদ দিয়ে ভালো চিন্তা ভাবনা কর। ঐটা একটা মশা ছিল।',
+        $handlerStack = HandlerStack::create($mock);
 
-        ];
+        $client = new Client(['handler' => $handlerStack]);
 
-        $jokes = new JokeFactory;
+        $jokes = new JokeFactory($client);
 
-        $joke = $jokes->getRandomJoke();
-
-        $this->assertContains($joke, $banglaJokes);
+        $this->assertSame('Project managers never ask Chuck Norris for estimations... ever.', $jokes->getRandomJoke());
     }
 }
